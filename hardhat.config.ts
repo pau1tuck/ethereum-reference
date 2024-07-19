1913061ee205c7ca054a48cd338036e08f0410b3
Paul Tuck <paul.tuck@oxon.org>
2024-07-19 12:26:43 +0700

    configure Hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import type { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+	solidity: "0.8.0",
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+	},
 };
 
 export default config;
